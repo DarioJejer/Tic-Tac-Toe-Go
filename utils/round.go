9ac200bcbed9d0d7f1ef9e15d3051a802dd3ab3a
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-08 21:22:49 -0300

    Added template for playRound funtionality

--- utils/round.go
+++ utils/round.go
@@ -1,13 +1,27 @@
 package utils
 
 type Result struct {
-	Message   string
-	Finnished bool
+	Message   string `json: message`
+	Finnished bool   `json: finnish`
 }
 
+const (
+	PLAYER   = 1
+	COMPUTER = 2
+)
+
+var MESSAGES = []string{"Keep playing", "You won", "Game over"}
+
 func PlayRound(buttonsSelected string) Result {
+	finnished, winner := determineWinner(buttonsSelected)
+	message := MESSAGES[winner]
+
 	var result Result
-	result.Message = "Keep playing!"
-	result.Finnished = false
+	result.Message = message
+	result.Finnished = finnished
 	return result
+}
+
+func determineWinner(buttonsSelected string) (bool, int) {
+	return false, 0
 }
